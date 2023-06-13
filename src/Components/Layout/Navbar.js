70a6a8727df7b86ec09b888738f1dc5c12ba9c8c
BerardiFr <f.berardi1994@hotmail.com>
2023-06-13 10:08:02 +0200

    update

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,6 +1,6 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FiShoppingCart } from "react-icons/fi";
-import { BiHomeAlt2 } from "react-icons/bi";
+import { BiHomeAlt2, BiLogInCircle } from "react-icons/bi";
 import { BsSearch } from "react-icons/bs";
 import { RiLogoutCircleRLine } from "react-icons/ri";
 import { FaUserCircle } from "react-icons/fa";
@@ -9,20 +9,27 @@ import { AuthContext } from "../Login/AuthContext";
 
 const Navbar = () => {
 
-    const {getCartSize, logout, userId} = useContext(AuthContext);
-    
+    const { getCartSize, logout, login, userId } = useContext(AuthContext);
+
     const [cartSizeLocal, setCartSizeLocal] = useState(getCartSize);
 
     const navigate = useNavigate();
 
     const handleLogout = () => {
         logout();
-        navigate("/login");
     }
 
-    useEffect(()=>{
+    const handleRegister = () => {
+        navigate("/register1");
+    }
+
+    useEffect(() => {
         setCartSizeLocal(getCartSize);
     }, [getCartSize]);
+
+    const handleLogin = () => {
+        navigate("/login");
+    }
 
     const location = useLocation();
     const hiddenNavbarRoutes = ['/login', '/register1', '/register2'];
@@ -40,15 +47,16 @@ const Navbar = () => {
                 </div>
                 <div className="list-container">
                     <ul className="list1">
-                        
+                        <li className="login-li"><button onClick={handleLogin}> <strong>Accedi</strong> </button></li>
+                        <li className="register-li"><button onClick={handleRegister}> <strong>Registrati</strong> </button></li>
                         <Link to={"/"}><li><strong><BiHomeAlt2 className="cart-icon" /></strong></li></Link>
+                        <li><FaUserCircle className="user-icon" /></li>
                         <li><strong><BsSearch className="search-icon" /></strong></li>
                         <Link to={"/cart"}><li className="cart-li"><FiShoppingCart className="cart-icon" />
                             <span className="cart-total"> {cartSizeLocal} </span>
                         </li></Link>
-                        <li><FaUserCircle className="user-icon"/></li>
                         <li className="logout-li" onClick={handleLogout}><RiLogoutCircleRLine className="logout-icon" /></li>
-                        
+
                     </ul>
                 </div>
             </div>
